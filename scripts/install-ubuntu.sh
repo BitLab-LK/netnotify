82be98789c0de57d0254a469bf96d18b4a5032bf
Chavindu Nuwanpriya <chavindun@gmail.com>
2026-08-06 13:55:35 +1000

    fix: use dynamic LISTEN_ADDRESS in write_netdata_helper

--- scripts/install-ubuntu.sh
+++ scripts/install-ubuntu.sh
@@ -234,17 +234,17 @@ EOF_SERVICE
 
 write_netdata_helper() {
   install -d -m 0755 "${NETDATA_NOTIFY_DIR}"
-  cat > "${NETDATA_NOTIFY_FILE}" <<'EOF_NETDATA'
+  cat > "${NETDATA_NOTIFY_FILE}" <<EOF_NETDATA
 #!/usr/bin/env sh
 set -eu
-: "${NETNOTIFY_URL:=http://127.0.0.1:8080/v1/sources/netdata}"
-curl -fsS -X POST "${NETNOTIFY_URL}" \
-  --data-urlencode "alarm=${alarm:-${ALARM:-}}" \
-  --data-urlencode "status=${status:-${STATUS:-}}" \
-  --data-urlencode "hostname=${hostname:-${HOSTNAME:-}}" \
-  --data-urlencode "chart=${chart:-${CHART:-}}" \
-  --data-urlencode "family=${family:-${FAMILY:-}}" \
-  --data-urlencode "summary=${info:-${INFO:-}}"
+: "\${NETNOTIFY_URL:=http://${LISTEN_ADDRESS}/v1/sources/netdata}"
+curl -fsS -X POST "\${NETNOTIFY_URL}" \
+  --data-urlencode "alarm=\${alarm:-\${ALARM:-}}" \
+  --data-urlencode "status=\${status:-\${STATUS:-}}" \
+  --data-urlencode "hostname=\${hostname:-\${HOSTNAME:-}}" \
+  --data-urlencode "chart=\${chart:-\${CHART:-}}" \
+  --data-urlencode "family=\${family:-\${FAMILY:-}}" \
+  --data-urlencode "summary=\${info:-\${INFO:-}}"
 EOF_NETDATA
   chmod 0755 "${NETDATA_NOTIFY_FILE}"
 }
